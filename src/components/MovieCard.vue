caaa4bb913e40a75ac4347e815898433244a4ae7
zachkont <z.kontoulis@gmail.com>
2019-09-23 10:50:10 +0300

    Styling stuff

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,16 +2,16 @@
   <v-flex>
     <v-card
       class="mx-auto"
-      max-width="344"
+      max-width="300"
       :color="movie.selected ? 'primary' : 'white'"
       v-on:click="clickHandler(movie)"
     >
       <v-card-text>
         <div>{{movie.room}}</div>
         <v-list-item-title class="subtitle-2 mb-1">{{movie.subtitle}}</v-list-item-title>
-        <p class="display-1 text--primary">{{movie.title}}</p>
+        <p class="title text--primary">{{movie.title}}</p>
         <p>{{movie.category}}</p>
-        <div class="text--primary">{{movie.description}}</div>
+        <div class="description text--primary">{{movie.description}}</div>
       </v-card-text>
       <v-card-actions>
         <v-btn text color="deep-purple accent-4">Learn More</v-btn>
@@ -19,6 +19,21 @@
     </v-card>
   </v-flex>
 </template>
+
+<style scoped>
+  .title {
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 1.5rem;
+    letter-spacing: 0.0073529412em !important;
+    font-family: "Roboto", sans-serif !important;
+  }
+
+  .description {
+    font-size: 16px;
+    font-weight: 400;
+  }
+</style>
 
 <script>
 import { mapActions } from "vuex";
